75aa94021fa5771b73b6494ff989cd2f0ff4e7b0
thinksttecheval <thinksttecheval@gmail.com>
2025-06-05 14:42:39 +0200

    update the webpage to be fully responsive and PDF downloadable

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/download-resume")
+def download_resume():
+    return send_from_directory(directory="static/files", path="Resume - Simphiwe Sithole.pdf", as_attachment=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8000)
